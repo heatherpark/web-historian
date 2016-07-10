dd0bfad5f23d5777574f8e6336a35f07542eb5cb
Heather Park <hthr.prk@gmail.com>
2016-07-09 23:05:13 -0700

    Implement function to handle GET requests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,40 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var fs = require('fs');
+var httpHelp = require('./http-helpers');
+
+var actions = {
+  'GET': handleGet,
+  'POST': handlePost
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var method = req.method;
+
+  if (method in actions) {
+    actions[method](req, res);
+  }
+};
+
+var handleGet = function(req, res) {
+  if (req.url === '/') {
+    var indexPath = archive.paths.siteAssets + '/index.html';
+    httpHelp.serveAssets(res, indexPath, function(err, data) {
+      res.end(data);
+    });
+  } else {
+    var archivedPath = archive.paths.archivedSites + req.url;
+    httpHelp.serveAssets(res, archivedPath, function(err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end();
+      } else {
+        res.end(data);
+      }
+    });
+  }
+};
+
+var handlePost = function(req, res) {
+
 };
